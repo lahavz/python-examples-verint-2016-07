1ad202b7fbea0845bd723c7bae415c4eefb750d1
lahavz <lahavzz@gmail.com>
2016-07-04 15:22:57 +0300

    Solution for: sol-python-verint.modules-lab/02_bonus.py

--- 06_modules_lab/02_bonus.py
+++ 06_modules_lab/02_bonus.py
@@ -1,10 +1,11 @@
-""" Write a program that reads 2 numbers from sys.argv
-and prints their sum.
-Bonus: Print error messages for invalid inputs.
+import sys
 
-To print error messages we'll have to use a concept not yet learned in the
-course, and which will only be presented later: Exceptions.
-We'll tap into python's error handling and change its default
-error message to something more meaningful.
-"""
+num1 = int(0)
+num2 = int(0)
 
+try:
+    num1 = int(sys.argv[1])
+    num2 = int(sys.argv[2])
+    print "Sum is %d" % (num1+num2)
+except:
+    print "Problematic Agruments"
