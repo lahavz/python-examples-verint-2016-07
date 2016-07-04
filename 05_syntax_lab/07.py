a06fbfb8f94265ae7bff5f57d815a2dfb6c88945
lahavz <lahavzz@gmail.com>
2016-07-04 15:21:49 +0300

    Solution for: sol-python-verint.syntax-lab/07.py

--- 05_syntax_lab/07.py
+++ 05_syntax_lab/07.py
@@ -1,8 +1,37 @@
-""" Write a program that selects a random number
-and asks the user to guess it.
+from random import randint
 
-After each guess print a hint "too large" or "too small" to the user.
+print "Exercise # 7"
 
-Bonus: To make things interesting, the program should cheat once in a white
-"""
+num = randint(1, 100)
 
+mistake_ratio = randint(5, 10)
+i = int(0)
+
+print "Number is: %d " % (num)
+
+
+while True:
+    print "What is your Guess?"
+    guess = int(raw_input())
+
+    if guess == num:
+        print "Its a Hit - Well Done!!!"
+        break
+    if i < mistake_ratio: 
+        if guess > num:
+            print "Try smaller number"
+        else:
+            print "Try higher number"
+            
+    if i == mistake_ratio: 
+        if guess > num:
+            print "Try higher number"
+        else:
+            print "Try smaller number"
+        i = int(-1)
+
+    i=i+1
+    
+    
+            
+    
