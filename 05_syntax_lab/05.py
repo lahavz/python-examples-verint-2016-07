d2d292791cf5c0b63298a2e25433b3e87f12d379
lahavz <lahavzz@gmail.com>
2016-07-05 09:18:10 +0300

    Update 05.py

--- 05_syntax_lab/05.py
+++ 05_syntax_lab/05.py
@@ -2,7 +2,7 @@ from random import randint
 
 print "Exercise # 5"
 
-
+"""Original Block
 while True:
     num = randint(1, 1000000)
     print "Number is %d" % (num)
@@ -17,6 +17,21 @@ while True:
                 print "Number %d is NOT divided by 13" % (num)
     else:
                 print "Number %d is NOT divided by 7" % (num)
+"""
+
+#New Block (fix with less indentation)
+while True:
+    num = randint(1, 1000000)
+    print "Number is %d" % (num)
+    if num % 7 > 0:
+        continue
+    elif num % 13 > 0:
+        continue
+    elif num % 15 > 0:
+        continue
+    else:
+        print "Number %d is divided by 7,13,15" % (num)
+        break
                 
         
 
