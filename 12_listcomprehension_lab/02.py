230834d5fc0dc42d85c7b185a20584a633b372b1
lahavz <lahavzz@gmail.com>
2016-07-12 21:04:41 +0300

    Solution for: sol-python-verint.list-comprehension-lab/02.py

--- 12_listcomprehension_lab/02.py
+++ 12_listcomprehension_lab/02.py
@@ -1,6 +1,14 @@
 """
-Write a python program that takes two words
-as sys.argv and prints only the letters
-common to both
+Using List comprehension and range functions - find the a-z letters
 """
+ 
+x = [chr(x) for x in range(256)]
 
+letters = [chr(j) for j in range(x.index('a'),x.index('z')+1)]
+
+print [a+b+c for a in letters for b in letters for c in letters]
+
+
+
+            
+    
