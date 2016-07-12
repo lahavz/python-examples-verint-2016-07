f2bab03129e06384315119e3db225dbc188fcd2b
lahavz <lahavzz@gmail.com>
2016-07-12 21:03:53 +0300

    Solution for: sol-python-verint.files-lab/01.py

--- 08_files_lab/01.py
+++ 08_files_lab/01.py
@@ -1,6 +1,15 @@
-"""
-Write a program that takes 2 file names
-and appends the second file's contents to
-the end of the first one
-"""
+#Program which gets two text files and append the first one to the second.
 
+import sys
+
+(src, dst) = sys.argv[1:]
+
+with open(src, "r") as fin:
+    with open(dst, "a") as fout:
+        fout.write('\n')
+        for line in fin:
+            fout.write(line)
+            
+
+            
+    
