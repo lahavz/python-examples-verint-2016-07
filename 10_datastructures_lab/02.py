8dbcc220960b5e6da1abbaed17f5bc20cc263048
lahavz <lahavzz@gmail.com>
2016-07-12 21:04:18 +0300

    Solution for: sol-python-verint.datastructures-lab/02.py

--- 10_datastructures_lab/02.py
+++ 10_datastructures_lab/02.py
@@ -1,5 +1,20 @@
 """
-Write a program that prints only unique values
-from sys.argv (without program name)
+Looks for anagrams and print the couples
 """
+from collections import Counter
+
+src = "C:\Users\lzeno\OneDrive\Code\Python Scripts\ToCode Course\Tasks\Data Structures\Anagram.txt"
+
+lines = open(src,"r").readlines()
+counters =[]
+
+for i in range(len(lines)):
+    lines[i] = lines[i].replace('\n','')
+
+for i in range(len(lines)):
+    line = Counter(lines[i])
+    for j in range(i+1,len(lines)):
+        if (line == Counter(lines[j])) and not line in counters:
+            print "%s : %s" % (lines[i], lines[j])
+            counters.append(Counter(lines[i]))
 
