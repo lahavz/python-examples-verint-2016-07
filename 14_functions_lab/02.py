a937f8fcf3780d59325ec7ef39f4bcc93cd82c45
lahavz <lahavzz@gmail.com>
2016-07-14 21:37:53 +0300

    Solution for: sol-python-verint.functions-lab/02.py

--- 14_functions_lab/02.py
+++ 14_functions_lab/02.py
@@ -1,7 +1,37 @@
 """
-Write a function that takes two arguments: 
-    A string
-    And a number
-If wrong types were passed in, raise an exception
+function that gets two paramets and check validity. (str and number)
 """
+from types import *
 
+
+
+def func(*args):
+    parameters = { 'integers' : 0, 'strings' : 0 }
+
+    for x in args:
+        if type(x) is int: parameters['integers'] += 1
+        if type(x) is str: parameters['strings'] += 1
+        
+    if (parameters['strings'] == 1) and (parameters['integers'] == 1):
+        print parameters
+        print 'ok'
+    else:
+        print parameters
+        print 'Not ok'
+        raise Exception("Wrong parameters")
+
+
+
+
+
+
+
+    
+
+func(5,"lahav")
+func(5,"ksks","lahav")
+ 
+
+
+            
+    
