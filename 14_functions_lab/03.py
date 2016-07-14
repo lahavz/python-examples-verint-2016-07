a04f599822301d6b66c513505c5387da758dd723
lahavz <lahavzz@gmail.com>
2016-07-14 21:37:55 +0300

    Solution for: sol-python-verint.functions-lab/03.py

--- 14_functions_lab/03.py
+++ 14_functions_lab/03.py
@@ -1,5 +1,23 @@
 """
-Write a function that calculates the sum
-of the 10th digit from all arguments passed to it
+function sums the tenth numbers
 """
+from types import *
 
+
+
+def func(*args):
+    res = 0
+    for x in args:
+        if (type(x) is int) and (int(x) >= 10): res += x / 10 % 10
+    print res
+
+
+    
+
+func(55,"lahav",43)
+func(5,"ksks","lahav")
+ 
+
+
+            
+    
