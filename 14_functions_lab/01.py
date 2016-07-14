d453466c30458955a2fb293af96921072f8d5b8f
lahavz <lahavzz@gmail.com>
2016-07-14 21:37:52 +0300

    Solution for: sol-python-verint.functions-lab/01.py

--- 14_functions_lab/01.py
+++ 14_functions_lab/01.py
@@ -1,7 +1,26 @@
 """
-Write 2 functions:
-    mysum - returns the sum of its input arguments
-    mymul - returns the multiplication of its input arguments
-    Ignore non-numeric arguments
+Two fucntions: 1st is sum , 2nd is multiply.
 """
 
+from types import *
+
+def sum(*args):
+    res = 0
+    for x in args:
+        if type(x) is int: res += x
+    return res
+
+def multiply(*args):
+    res = 1
+    for x in args:
+        if type(x) is int: res *= x
+    return res
+
+
+print sum(5,6,7,"lahav")
+print multiply(5,6,7,"lahav")
+ 
+
+
+            
+    
