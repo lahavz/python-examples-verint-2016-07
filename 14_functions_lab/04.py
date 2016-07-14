87bb282b62c39029747e261de5db33496e29ee6a
lahavz <lahavzz@gmail.com>
2016-07-14 21:37:56 +0300

    Solution for: sol-python-verint.functions-lab/04.py

--- 14_functions_lab/04.py
+++ 14_functions_lab/04.py
@@ -1,6 +1,20 @@
 """
-Write a function that takes minlen and
-a list of words, and returns only the words
-longer than minlen
+function return words greather than length
 """
+from types import *
 
+
+
+def func(length,*args):
+    res = []
+    for x in args:
+        if (type(x) is str) and (len(x) >= length): res.append(x)
+    print res
+
+func(2,"lahav","l","lala","bb",5)
+func(5,"ksks","lahav")
+     
+
+
+            
+    
