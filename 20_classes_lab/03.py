f8396ee9816a023fa28f68eadda2b81e9d133a4e
lahavz <lahavzz@gmail.com>
2016-07-21 15:32:03 +0300

    Solution for: sol-python-verint.class-syntax-lab/03.py

--- 20_classes_lab/03.py
+++ 20_classes_lab/03.py
@@ -1,13 +1,66 @@
 """
-Following code assumes a class named Widget that depends on
-other widgets
-Widget ctor takes a name, and function 
-add_dependency(depends)
-marks widget as depends on all widgets in "depends" list
-
-When building a widget, you need to first build all the widgets it
-depends on.
-
-Implement widget so the following code works
+Fill the code to print the result
 """
 
+class Widget(object):
+
+    widget_list = []
+    
+    def __init__(self,name = ""):
+        self._name = name
+        self._dependency = []
+
+    def add_dependency(self, *args):
+        for x in args:
+            self._dependency.append(x)
+
+
+
+    def create(self):     
+        if (len(self._dependency) > 0):
+            for a in self._dependency:
+                if a._name not in Widget.widget_list:
+                    a.create()
+        else:
+            if self._name not in Widget.widget_list:
+               Widget.widget_list.append(self._name)
+
+        if self._name not in Widget.widget_list:
+               Widget.widget_list.append(self._name)
+   
+
+
+
+    def build(self):
+        for x in self._dependency:
+            x.create()
+
+        for y in Widget.widget_list:
+            print y + ',',
+
+
+
+#Original Code
+
+luke    = Widget("Luke")
+hansolo = Widget("Han Solo")
+leia    = Widget("Leia")
+yoda    = Widget("Yoda")
+padme   = Widget("Padme Amidala")
+anakin  = Widget("Anakin Skywalker")
+obi     = Widget("Obi-Wan")
+darth   = Widget("Darth Vader")
+_all    = Widget("All")
+
+
+luke.add_dependency(hansolo, leia, yoda)
+leia.add_dependency(padme, anakin)
+obi.add_dependency(yoda)
+darth.add_dependency(anakin)
+
+_all.add_dependency(luke, hansolo, leia, yoda, padme, anakin, obi, darth)
+_all.build()
+
+
+# code should print: Han Solo, Padme Amidala, Anakin Skywalker, Leia, Yoda, Luke, Obi-Wan, Darth Vader
+# (can print with newlines in between modules)
